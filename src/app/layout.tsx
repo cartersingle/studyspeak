93904174ae31a2dee669862c54aa6e180481b89b
Carter Singletary <120229354+cartersingle@users.noreply.github.com>
2024-03-01 18:02:30 -0700

    add react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -23,7 +24,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
