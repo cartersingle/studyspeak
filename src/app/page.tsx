1cf7de90eb23cb2da7ed759f7d94520671d01a77
Carter Singletary <120229354+cartersingle@users.noreply.github.com>
2024-03-01 18:31:33 -0700

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { MoveRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <Button>Hello World</Button>
-    </main>
+    <>
+      <div className=" max-w-xl mx-auto space-y-8">
+        <h1 className="text-7xl tracking-tight">
+          The <span className="font-bold">AI</span> powered study app that{" "}
+          <span className="underline">Superpowers</span> your study habits.
+        </h1>
+        <Button size="lg" asChild>
+          <Link href="/login">
+            See the Difference
+            <MoveRight className="ml-2" />
+          </Link>
+        </Button>
+      </div>
+    </>
   );
 }
